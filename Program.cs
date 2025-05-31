fda18f23d88e25a1c970b76994e3237c78c8998a
George <giazeek@gmail.com>
2025-05-31 22:38:42 +0300

    skeleton for .NET back

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDB")); // look up at appsettings for MongoDB settings . 
+
+builder.Services.AddSingleton<IMongoClient>(s =>
+    new MongoClient(builder.Configuration.GetSection("MongoDB:ConnectionString").Value));
+// adding to config mongo settings 
+
+
+// 👇 Register services here
+builder.Services.AddControllers(); // adds connection to ur apies . 
+
+builder.Services.AddSingleton<ProductService>();// adding our handeling layer . 
+
+
+
+var app = builder.Build();
+
+
+// Middleware
+app.UseAuthentication();  // 🔑 Check who the user is (token, etc.)
+app.UseAuthorization();   // 🛂 Check if user is allowed to access specific route
+
+
+app.MapControllers();    // Map controller routes to app 
+
+app.Run();
---
+++ Services/ProductService.cs
@@ -0,0 +1,32 @@
+using ArmyStockApp.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ArmyStockApp.Services
+{
+    public class ProductService
+    {
+        private readonly IMongoCollection<Product> _products;
+
+        public ProductService(IOptions<MongoDbSettings> settings, IMongoClient client)
+        {
+            var db = client.GetDatabase(settings.Value.DatabaseName);
+            _products = db.GetCollection<Product>("Products");
+        }
+
+        public async Task<List<Product>> GetAsync() =>
+            await _products.Find(_ => true).ToListAsync();
+
+        public async Task<Product?> GetByIdAsync(string id) =>
+            await _products.Find(p => p.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Product product) =>
+            await _products.InsertOneAsync(product);
+
+        public async Task UpdateAsync(string id, Product updated) =>
+            await _products.ReplaceOneAsync(p => p.Id == id, updated);
+
+        public async Task DeleteAsync(string id) =>
+            await _products.DeleteOneAsync(p => p.Id == id);
+    }
+}
